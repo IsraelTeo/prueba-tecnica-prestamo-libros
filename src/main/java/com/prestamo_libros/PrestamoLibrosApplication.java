afe81468666c183384cc641206dafa85b35bcc37
Israel <israel.bastion123@gmail.com>
2025-07-21 20:29:41 -0500

    docs: read.me added

---
+++ src/main/java/com/prestamo_libros/PrestamoLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.prestamo_libros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestamoLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestamoLibrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prestamo_libros/PrestamoLibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prestamo_libros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamoLibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
